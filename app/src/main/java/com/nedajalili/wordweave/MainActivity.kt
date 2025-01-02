a060dea9a5e2662a31890c1745c2571ea837df57
neda jalili <neda.jalili.23@gmail.com>
2025-01-02 23:37:29 +0330

    Implemented main UI using Jetpack Compose with a centralized Start Game button. 😊

--- app/src/main/java/com/nedajalili/wordweave/MainActivity.kt
+++ app/src/main/java/com/nedajalili/wordweave/MainActivity.kt
@@ -3,45 +3,35 @@ package com.nedajalili.wordweave
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.nedajalili.wordweave.ui.theme.WordWeaveTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             WordWeaveTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                StartGameScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WordWeaveTheme {
-        Greeting("Android")
+fun StartGameScreen() {
+    // طراحی صفحه اصلی با یک دکمه در مرکز
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Button(onClick = { /* عمل مربوط به دکمه */ }) {
+            Text("Start Game")
+        }
     }
 }
